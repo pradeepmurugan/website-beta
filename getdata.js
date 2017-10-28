d424a79df52cd90c54baa4bc6fe27dc5d2c99c62
Pradeep Murugan <pradeepmurugan10@gmail.com>
2017-10-29 05:09:52 +0530

    Create getdata.js

---
+++ getdata.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    $("#table").hide();
+    $("button").click(function(){
+    $("#table").show();
+     var arr = new Array();
+     function callback(data)
+     {
+        arr.push(data);
+        $('#table').bootstrapTable({ data: arr });
+        $('#table').bootstrapTable('refreshOptions', { data: arr });
+     }
+     $.get("https://jsonplaceholder.typicode.com/posts/1", callback);
+     $.get("https://jsonplaceholder.typicode.com/posts/2", callback);
+     $.get("https://jsonplaceholder.typicode.com/posts/3", callback);
+     $.get("https://jsonplaceholder.typicode.com/posts/4", callback);
+     $.get("https://jsonplaceholder.typicode.com/posts/5", callback);
+     console.log("hello");
+    });
+});
