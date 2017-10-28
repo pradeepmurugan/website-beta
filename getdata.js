a1af9a31aa76d1c0b8f3496ab9063b563f3dd776
Pradeep Murugan <pradeepmurugan10@gmail.com>
2017-10-29 05:27:26 +0530

    Add a function

--- getdata.js
+++ getdata.js
@@ -1,3 +1,5 @@
+function clickfunction{
+    
 $(document).ready(function(){
     $("#table").hide();
     $("button").click(function(){
@@ -17,3 +19,4 @@ $(document).ready(function(){
      console.log("hello");
     });
 });
+}
