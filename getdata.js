454cf6976e3228bec9a05598d68779b159d449a2
Pradeep Murugan <pradeepmurugan10@gmail.com>
2017-12-17 13:00:18 +0530

    Update getdata.js

--- getdata.js
+++ getdata.js
@@ -2,26 +2,27 @@ $(document).ready(function () {
     $("#createform").hide();
     $("#create").click(function () {
         $("#createform").show();
-        var contextroot = ""
-        $("#createform").submit(function (e) {
-            e.preventDefault();
-            var form = $(this);
-            var action = form.attr("action");
-            var data = form.serializeArray();
-            $.ajax({
-                url: contextroot + action,
-                dataType: 'json',
-                type: 'POST',
-                contentType: 'application/json',
-                data: JSON.stringify(getFormData(data)),
-                success: function (data) {
-                    console.log("DATA POSTED SUCCESSFULLY" + data);
-                },
-                error: function (jqXhr, textStatus, errorThrown) {
-                    console.log(errorThrown);
-                }
-            });
+    });
+    var contextroot = ""
+    $("#createform").submit(function (e) {
+        e.preventDefault();
+        var form = $(this);
+        var action = form.attr("action");
+        var data = form.serializeArray();
+        $.ajax({
+            url: contextroot + action,
+            dataType: 'json',
+            type: 'POST',
+            contentType: 'application/json',
+            data: JSON.stringify(getFormData(data)),
+            success: function (data) {
+                console.log("DATA POSTED SUCCESSFULLY" + data);
+            },
+            error: function (jqXhr, textStatus, errorThrown) {
+                console.log(errorThrown);
+            }
         });
     });
+
 });
 
