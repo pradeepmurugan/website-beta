8e31f1060dada940008a6edc8d084a2641312346
Pradeep Murugan <pradeepmurugan10@gmail.com>
2017-12-17 13:21:47 +0530

    Update getdata.js

--- getdata.js
+++ getdata.js
@@ -1,9 +1,58 @@
 $(document).ready(function () {
     $("#createform").hide();
+    $("#removeform").hide();
+    $("#displayform").hide()
     $("#create").click(function () {
         $("#createform").show();
         var contextroot = ""
-        $("#createform").submit(function (e) {
+        $("#form").submit(function (e) {
+            e.preventDefault();
+            var form = $(this);
+            var action = form.attr("action");
+            var data = form.serializeArray();
+            $.ajax({
+                url: contextroot + action,
+                dataType: 'json',
+                type: 'POST',
+                contentType: 'application/json',
+                data: JSON.stringify(getFormData(data)),
+                success: function (data) {
+                    console.log("DATA POSTED SUCCESSFULLY" + data);
+                },
+                error: function (jqXhr, textStatus, errorThrown) {
+                    console.log(errorThrown);
+                }
+            });
+        });
+    });
+    
+    $("#remove").click(function () {
+        $("#removeform").show();
+        var contextroot = ""
+        $("#form").submit(function (e) {
+            e.preventDefault();
+            var form = $(this);
+            var action = form.attr("action");
+            var data = form.serializeArray();
+            $.ajax({
+                url: contextroot + action,
+                dataType: 'json',
+                type: 'POST',
+                contentType: 'application/json',
+                data: JSON.stringify(getFormData(data)),
+                success: function (data) {
+                    console.log("DATA POSTED SUCCESSFULLY" + data);
+                },
+                error: function (jqXhr, textStatus, errorThrown) {
+                    console.log(errorThrown);
+                }
+            });
+        });
+    });
+    $("#display").click(function () {
+        $("#displayform").show();
+        var contextroot = ""
+        $("#form").submit(function (e) {
             e.preventDefault();
             var form = $(this);
             var action = form.attr("action");
